9c812bb9452d757e45dc0a0394648c9d3a938bf4
Fabiano <fabianotmp@gmail.com>
2024-11-09 00:42:12 -0300

    tela de login com backgound

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../../services/cliente.service';
 import { Cliente } from '../../models/cliente';
 
-
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -12,11 +11,15 @@ import { Cliente } from '../../models/cliente';
 })
 export class DashboardComponent implements OnInit {
   totalClientes: number = 0;
+  saudacao: string = '';
+  dataAtual: string = '';
 
   constructor(private clienteService: ClienteService) {}
 
   ngOnInit(): void {
     this.carregarClientes();
+    this.saudacao = this.obterSaudacao();
+    this.dataAtual = this.obterDataFormatada();
   }
 
   carregarClientes(): void {
@@ -24,10 +27,36 @@ export class DashboardComponent implements OnInit {
       (clientes: Cliente[]) => {
         this.totalClientes = clientes.length;
       },
-      (error: any) => { // Adicione a anotação de tipo aqui
-        console.error('Erro ao carregar clientes', error);
+      (erro: any) => {
+        console.error('Erro ao carregar clientes', erro);
       }
     );
   }
-}
 
+  obterSaudacao(): string {
+    const agora = new Date();
+    const horas = agora.getHours();
+
+    if (horas >= 5 && horas < 12) {
+      return 'Bom dia';
+    } else if (horas >= 12 && horas < 18) {
+      return 'Boa tarde';
+    } else {
+      return 'Boa noite';
+    }
+  }
+
+  obterDataFormatada(): string {
+    const hoje = new Date();
+    const dia = hoje.getDate();
+    const ano = hoje.getFullYear();
+
+    const meses = [
+      'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 
+      'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+    ];
+    const mes = meses[hoje.getMonth()];
+
+    return `${this.saudacao}, hoje é ${dia} de ${mes} de ${ano}`;
+  }
+}
