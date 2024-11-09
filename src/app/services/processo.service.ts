5c47e12447fb78d51512f9a45ee7849424d4466d
Pedro Soares <soarespedroo357@gmail.com>
2024-11-08 22:08:57 -0300

    commit

---
+++ src/app/services/processo.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Processo } from '../models/processo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProcessoService {
+
+  http = inject(HttpClient);
+  API = "http://localhost:8080/api/processo"
+
+  constructor() { }
+
+  findAll(): Observable<Processo[]>{
+    return this.http.get<Processo[]>(this.API+"/findAll");
+  }
+  delete(id:number): Observable<void>{
+    return this.http.delete<void>(this.API+"/delete/"+id);
+  }
+  save(processo:Processo): Observable<Processo>{
+    return this.http.post<Processo>(this.API+"/save", processo);
+  }
+  update(id:number , processo:Processo): Observable<Processo>{
+    return this.http.put<Processo>(this.API+"/update/"+ id, processo);
+  }
+  findById(id:number): Observable<Processo>{
+    return this.http.get<Processo>(this.API+"/findById/"+ id);
+  }
+  findByNumero(numero : string): Observable<Processo>{
+    return this.http.get<Processo>(this.API+"/findByNumeroProcesso"+ numero);
+  }
+
+}
