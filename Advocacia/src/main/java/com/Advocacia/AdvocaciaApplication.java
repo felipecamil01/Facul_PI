4b3e4e2520860a87373ed3ccf6600029a2edb988
Pedro Soares <soarespedroo357@gmail.com>
2025-06-22 16:09:28 -0300

    Update AdvocaciaApplication.java

--- Advocacia/src/main/java/com/Advocacia/AdvocaciaApplication.java
+++ Advocacia/src/main/java/com/Advocacia/AdvocaciaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@SpringBootApplicationtinha 
+@SpringBootApplication
 public class AdvocaciaApplication {
 
 	public static void main(String[] args) {
